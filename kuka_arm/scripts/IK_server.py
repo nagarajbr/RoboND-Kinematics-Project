c22c1c24ce00a7c0b230182f6d6bd5bf6a8fe674
Nagaraj Bettadapura <nagaraj.bettadapura@gmail.com>
2017-07-14 22:11:00 -0500

    Update IK_server.py

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -170,6 +170,19 @@ def handle_calculate_IK(req):
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
             joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
             joint_trajectory_list.append(joint_trajectory_point)
+                             
+            #### Step 4
+                 
+            T0_3 = simplify(T0_1 * T1_2 * T2_3)
+            R0_3 = T0_3[0:3,0:3]
+            R3_6 = R0_3.transpose() *  Matrix(R0_g) * R_corr.transpose()
+            R3_6 = R3_6.subs({q1: theta1, q2:theta2, q3: theta3})
+            
+            #### Step 5
+                             
+            theta4 = atan2(R3_6[2,2], -R3_6[0, 2]).evalf() 
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0]).evalf() 
+            theta5 = atan2(sqrt(R3_6[0, 2]*R3_6[0, 2] + R3_6[2, 2]*R3_6[2, 2]), R3_6[1, 2]).evalf()
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
